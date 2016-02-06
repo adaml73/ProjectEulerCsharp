b6b2f41afc1d37fdf1c28742aed8556941011922
adaml73 <adamlyng73@gmail.com>
2016-02-06 16:30:57 -0500

    Project Euler Problem 1
    C# - attempt of the First Project Euler problem
    - List & sum multiples of 3 or 5 below 1000

---
+++ ProjEuler1/ProjEuler1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjEuler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intMax = 1000; 
+            int intCounter = 0; 
+            int intAnswer = 0; 
+            
+           
+            
+            while (intCounter < intMax)
+            {
+                if ((intCounter % 3 == 0) || (intCounter % 5 == 0) == true)
+                {
+                    Console.WriteLine(intCounter);
+                    intAnswer = intAnswer + intCounter;
+                    ++intCounter;
+                }
+                else
+                {
+                    ++intCounter;
+                }
+
+              
+               
+            }
+
+            Console.WriteLine(intAnswer);
+            Console.ReadLine();
+        }
+    }
+}
